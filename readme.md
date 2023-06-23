74fcf1c593f0a10fba05b96aebffb5406c3fd1e8
Carrie White <yukidama@comcast.net>
2023-06-22 23:12:29 -0400

    add basic message generator

---
+++ readme.md
@@ -0,0 +1,8 @@
+Add readme.md with a description of the project
+____
+SPADE is a user application that generates grounding techniques and affirmations. The user can select specific categories or select random where the program will decide for them.
+
+
+
+
+no known issues to track
---
+++ spade.py
@@ -0,0 +1,64 @@
+"""
+SPADE: Grounding Techniques
+
+
+"""
+
+import random
+import time
+
+
+
+print("Welcome to SPADE")
+
+
+
+#User category selector
+"""
+print("Categories: Environment, Body")
+userCategory = input("Please select a category: ").lower()
+
+def getType(userCategory):
+	if userCategory == 
+
+print(userCategory)
+"""
+
+#basic grounding prompt generator
+#Library for auto messages
+numList = ["one","two","three","four"]
+shapeList = ["circle","square","triangle","rectangle"]
+colorList = ["red","orange", "yellow", "green","blue","purple","pink","black","white","gray"]
+adjList = ["beautiful","cozy","happy","warm","restful","cute","delicate"]
+
+#Assignments for each variable
+num1 = random.choice(numList)
+num2 = random.choice(numList)
+num3 = random.choice(numList)
+num4 = random.choice(numList)
+num5 = random.choice(numList)
+num6 = random.choice(numList)
+shape1 = random.choice(shapeList)
+color1 = random.choice(colorList)
+adj1 = random.choice(adjList)
+adj2 = random.choice(adjList)
+
+
+#Slow down the wall of text
+time.sleep(3)
+
+
+
+print("Look around your space and see if you can find", num1, "things that have a", shape1,"on them.")
+time.sleep(15)
+print("Can you find",num2, "things that have the color", color1, "on them?")
+time.sleep(15)
+print("Can you find", num3, "things that have at least", num4, "colors on them?")
+time.sleep(15)
+print("How about", num5, "things that are", adj1, "to you in some way?")
+time.sleep(15)
+print("What about", num6, "things that are", adj2, "to you?")
+time.sleep(15)
+print("See if you can identify one thing that you can do right now that will help you feel 1% better and do that thing right now.")
+print("(Giving yourself a little message, taking a deep breath, having a sip of water, getting a snack)")
+
